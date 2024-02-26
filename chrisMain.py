1a78c0663455b3319664922127bdc9799a9b9084
ChrisPerezLebron <98716656+ChrisPerezLebron@users.noreply.github.com>
2024-02-26 17:59:52 -0500

    able to get posts in search results as a list

--- chrisMain.py
+++ chrisMain.py
@@ -1,9 +1,20 @@
 import requests 
 
 
-
-result = requests.get(url ="https://www.reddit.com/search/.json?q=trump") 
+#send a get request to the json endpoint 
+result = requests.get(url ="https://www.reddit.com/search/.json?q=trump").json()
 
 
 #prints resulting json for search page
-print(result.json())
+
+ 
+print(result)
+
+print(type(result))
+
+
+#try to iterate through posts in search results 
+children = result.get("data").get("children")
+
+print(children)
+print(type(children))
