5b433590c46d08f2e079f1d2175d4e1247fb0b34
ChrisPerezLebron <98716656+ChrisPerezLebron@users.noreply.github.com>
2024-02-26 18:02:39 -0500

    able to get child links for search result

--- chrisMain.py
+++ chrisMain.py
@@ -6,15 +6,16 @@ result = requests.get(url ="https://www.reddit.com/search/.json?q=trump").json()
 
 
 #prints resulting json for search page
-
- 
-print(result)
-
-print(type(result))
+# print(result)
+# print(type(result))
 
 
 #try to iterate through posts in search results 
 children = result.get("data").get("children")
 
-print(children)
-print(type(children))
+for child in children: 
+    print(child.get("data").get("permalink"))
+    print()
+
+# print(children)
+# print(type(children))
