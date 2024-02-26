46e6f409fcd7f6a58a9d41a4c9dc29b697950a3b
ChrisPerezLebron <98716656+ChrisPerezLebron@users.noreply.github.com>
2024-02-26 18:18:48 -0500

    created function to handle getting post links from search query

--- chrisMain.py
+++ chrisMain.py
@@ -1,21 +1,27 @@
 import requests 
 
 
-#send a get request to the json endpoint 
-result = requests.get(url ="https://www.reddit.com/search/.json?q=trump").json()
+#returns list of reddit links posts related to the given search term
+#n defines the number of posts we want to retrieve 
+    #TODO: implement n so we can get n posts
+def search(term, n): 
+    links = []
+
+    #the link we will be sending a GET request to
+    link = f"https://www.reddit.com/search/.json?q={term}"
+    # print(link)
+
+    result = requests.get(url =link,  headers = {'User-agent': 'your bot 0.1'}).json()
+    # print(result)
+    #just incase we got a bad response, just try again 
+
+    
+    children = result.get("data").get("children")
+
+    for child in children: 
+        links.append("https://www.reddit.com/"+child.get("data").get("permalink"))
+    
+    return links
 
 
-#prints resulting json for search page
-# print(result)
-# print(type(result))
-
-
-#try to iterate through posts in search results 
-children = result.get("data").get("children")
-
-for child in children: 
-    print(child.get("data").get("permalink"))
-    print()
-
-# print(children)
-# print(type(children))
+print(len(search("wowPosts", 1)))
