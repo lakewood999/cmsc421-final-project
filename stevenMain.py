0be7fe1a99489c3d4961112a7b7b5907b5e39969
Steven Su <lakewood999@gmail.com>
2024-04-06 09:59:17 -0400

    Update comment retrieval

--- stevenMain.py
+++ stevenMain.py
@@ -1,5 +1,8 @@
-from more_itertools import last
-import praw, dotenv, os
+from ast import Tuple
+from dataclasses import dataclass, field
+from multiprocessing import heap
+from typing import List, Tuple
+import praw, praw.models, dotenv, os, heapq, json
 import pandas as pd
 from flair.data import Sentence
 from flair.nn import Classifier
@@ -23,25 +26,44 @@ reddit = praw.Reddit(
     password=os.getenv('REDDIT_PASSWORD')
 )
 
-def expand_comments(submission, limit: int = 3) -> list:
-    """Expands a Reddit submission to include its comments.
+@dataclass(order=True)
+class PrioritizedComment:
+    priority: int
+    comment: praw.models.Comment | praw.models.MoreComments = field(compare=False)
+    parent: str = field(compare=False)
+
+def expand_comments(submission, limit: int = 3, total_limit: int = -1) -> list:
+    """Expands a Reddit submission to include its comments up to an optional depth limit.
+    Also allows a total comment limit which will stop expanding comments once reached, no matter
+    the depth or overall progress.
 
     :param praw.models.Submission submission: The submission to expand.
+    :param int limit: The depth limit of comments to explore. Defaults to 3.
+    :param int total_limit: The total number of comments to include. Defaults to -1 (no limit).
     :return: A list containing the submission and its comments.
     """
-    # Expand the submission
-    submission.comments.replace_more(limit=limit)
-    # Extract the comments
-    comments = submission.comments.list()
-    # Parse the comments
-    comments = [
-        {
-            'body': comment.body,
-            'url': comment.permalink,
-            'score': comment.score
-        } for comment in comments if comment.body != '[removed]' and comment.body != '[deleted]'
-    ]
-    return comments
+    # Queue will be in format of (comment, depth, parent)
+    final_comments = []
+    comments_queue: List[PrioritizedComment] = [PrioritizedComment(1, comment, "post") for comment in submission.comments.list()]  # Seed with top-level comments
+    while len(comments_queue) > 0 and (total_limit == -1 or len(final_comments) < total_limit):
+        c = heapq.heappop(comments_queue)
+        depth, comment, parent = c.priority, c.comment, c.parent
+        if depth > limit:
+            continue
+        if isinstance(comment, praw.models.Comment):
+            if not (comment.body == '[removed]' or comment.body == '[deleted]'):
+                final_comments.append({
+                    'body': comment.body,
+                    'url': comment.permalink,
+                    'score': comment.score,
+                    'parent': parent,
+                })
+                comments_queue.extend([PrioritizedComment(depth + 1, reply, comment.id) for reply in comment.replies.list()])
+                heapq.heapify(comments_queue)
+        elif isinstance(comment, praw.models.MoreComments):
+            comments_queue.extend([PrioritizedComment(depth, reply, parent) for reply in comment.comments()])
+            heapq.heapify(comments_queue)
+    return final_comments
 
 def topic_search(query: str, subreddit: str, target_posts: int = 10, include_comments: bool = False, prefer_recent: bool = True) -> list:
     """Uses the Reddit API to search for posts related to a given query.
@@ -54,7 +76,7 @@ def topic_search(query: str, subreddit: str, target_posts: int = 10, include_com
     :return: A list of processed search results
     """
     search_results = []
-    num_posts_to_search = int(target_posts * 2)  # Search more posts than needed to account for removed/deleted posts
+    num_posts_to_search = int(target_posts * 5)  # Search more posts than needed to account for removed/deleted posts
     kwargs = {
         'limit': num_posts_to_search,
         'sort': 'new' if prefer_recent else 'relevance'
@@ -68,13 +90,14 @@ def topic_search(query: str, subreddit: str, target_posts: int = 10, include_com
         if submission.selftext == '[removed]' or submission.selftext == '[deleted]':
             continue
         # Include only self-posts
-        if not submission.is_self:
+        if submission.is_self:
             continue
         # Drop nsfw
         if submission.over_18:
             continue
         # Add the post to the search results
         search_results.append({
+            'subreddit': submission.subreddit.display_name.lower(),
             'title': submission.title,
             'url': submission.permalink,
             'score': submission.score,
@@ -110,6 +133,8 @@ def sentiment_analysis(df: pd.DataFrame, column: str = 'body') -> pd.DataFrame:
     :param str column: The column to perform sentiment analysis on. Defaults to 'body'.
     :return: The original DataFrame with an additional column containing the sentiment analysis results.
     """
+    # Drop any rows with empty bodies
+    df = df[df[column] != '']
     # Convert body to Flair sentences
     df['sentence'] = df[column].apply(lambda x: Sentence(x)) # type: ignore
     # Perform sentiment analysis
@@ -121,7 +146,10 @@ def sentiment_analysis(df: pd.DataFrame, column: str = 'body') -> pd.DataFrame:
     return df
 
 # Example usage
-results = topic_search('donald trump judgement', 'politics', 5, True)
+results = topic_search('donald trump trial', 'politics', 5, True)
+json.dump(results, open('results.json', 'w'), indent=4)
+input('Results saved. Press enter to continue with analysis...')
 df = results_to_dataframe(results)
 df = sentiment_analysis(df)
+df.to_csv('results.csv', index=False)
 print(df.head(n=30))
