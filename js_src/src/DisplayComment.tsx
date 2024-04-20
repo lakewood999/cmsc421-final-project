fd0045a3fed9f21770ef8eed3b1368d24ecf53cf
Steven Su <lakewood999@gmail.com>
2024-04-19 23:44:07 -0400

    add markdown rendering for reddit content that uses it and some visual updates

--- js_src/src/DisplayComment.tsx
+++ js_src/src/DisplayComment.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import useDataStore from "./datastore";
 import { dataToNested } from "./helpers";
+import Markdown from 'react-markdown'
 
 const DisplayComment = (props: { commentId: string }) => {
-  const data = useDataStore((state) => state.redditData);
-  const { nested, parentData } = dataToNested(data);
+    const data = useDataStore((state) => state.redditData);
+    const { nested, parentData } = dataToNested(data);
 
-  const comment = parentData[props.commentId];
-  const children = nested[props.commentId];
+    const comment = parentData[props.commentId];
+    const children = nested[props.commentId];
 
-  let renderedChildren: React.JSX.Element[] = [];
-  if (children !== undefined) {
-    renderedChildren = children.map((child) => {
-      return <DisplayComment commentId={child.id} />;
-    });
-  }
+    let renderedChildren: React.JSX.Element[] = [];
+    if (children !== undefined) {
+        renderedChildren = children.map((child) => {
+            return <DisplayComment commentId={child.id} />;
+        });
+    }
 
-  return (
-    <div className="ms-3 ps-2" style={{ borderLeft: "1px solid black" }}>
-      <p>{comment.body}</p>
-      <div>
-        {renderedChildren}
-      </div>
-    </div>
-  );
+    return (
+        <div className="ms-3 ps-2" style={{ borderLeft: "1px solid black" }}>
+            <p><Markdown>{comment.body}</Markdown></p>
+            <div>
+                {renderedChildren}
+            </div>
+        </div>
+    );
 };
 
 export default DisplayComment;
--- js_src/src/DisplayPost.tsx
+++ js_src/src/DisplayPost.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import useDataStore from "./datastore";
 import { dataToNested } from "./helpers";
 import DisplayComment from "./DisplayComment";
+import Markdown from 'react-markdown'
 
 const DisplayPost = (props: { postId: string }) => {
     const data = useDataStore((state) => state.redditData)
-    const {nested, parentData} = dataToNested(data);
+    const { nested, parentData } = dataToNested(data);
 
     const post = parentData[props.postId]
     const children = nested[props.postId]
@@ -13,15 +14,21 @@ const DisplayPost = (props: { postId: string }) => {
     console.log("Rendering post", post.id, children.length);
 
     return (
-        <div>
-            <h2><a href={`https://reddit.com${post.url}`}>{post.title}</a></h2>
-            <p>{post.body}</p>
-            <hr/>
-            {children.map((child) => {
-                return (
-                    <DisplayComment commentId={child.id} />
-                )
-            })}
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">
+                    <a href={`https://reddit.com${post.url}`}>{post.title}</a>
+                </h5>
+                <div className="card-text">
+                    <Markdown>{post.body}</Markdown>
+                </div>
+                <hr />
+                {children.map((child) => {
+                    return (
+                        <DisplayComment commentId={child.id} />
+                    )
+                })}
+            </div>
         </div>
     )
 }
