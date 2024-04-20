4338e44c710fd8d6942d6b7f66981dffa37d04d9
Brady Egleston <135048000+begleston@users.noreply.github.com>
2024-04-20 01:02:47 -0400

    Forgot the datastore
    Whoops

--- js_src/src/datastore.js
+++ js_src/src/datastore.js
@@ -3,6 +3,8 @@ import {create} from 'zustand';
 const useDataStore = create((set, get) => ({
     redditData: null,
     setRedditData: (data) => set({redditData: data}),
+    sentData: null,
+    setSentData: (data) => set({sentData: data})
 }));
 
 export default useDataStore;
