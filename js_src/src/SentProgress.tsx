4e38f15a8cabdb869914d35ee4d0a7089eeef64e
Steven Su <lakewood999@gmail.com>
2024-05-01 14:34:55 -0400

    Visual updates, bug fixes, and corrections to sentiment progress

--- js_src/src/SentProgress.tsx
+++ js_src/src/SentProgress.tsx
@@ -6,17 +6,17 @@ const SentProgress = () => {
     const redditData = useDataStore((state) => state.redditData);
     const numModes = useDataStore((state) => state.numModes);
 
-    let body = [<div className="progress"><div className="progress-bar" role="progressbar" aria-valuenow={0} aria-valuemin={0} aria-valuemax={100}></div></div>];
+    let body = [<div className="progress"><div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow={0} aria-valuemin={0} aria-valuemax={100}></div></div>];
     if (redditData === null) {
         body
     } else {
         const total = redditData.length * numModes;
         // completed is the number of keys per entry in sentimentData
         const completed = Object.keys(sentimentData).reduce((acc: number, entry: string) => acc + Object.keys(sentimentData[entry]).length, 0);
-        console.log(total)
-        console.log(completed)
-        const prog: number = completed / total;
-        body = [<div className="progress"><div className="progress-bar" role="progressbar" style={{ width: `${prog}%` }} aria-valuenow={prog} aria-valuemin={0} aria-valuemax={100}></div></div>];
+        // count the number of entries with an empty body in redditData
+        const empty = redditData.filter((entry: any) => entry.body === "").length * numModes;
+        const prog: number = completed / (total - empty);
+        body = [<div className="progress"><div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style={{ width: `${prog * 100}%` }} aria-valuenow={prog} aria-valuemin={0} aria-valuemax={100}>{`${Math.round((Math.abs(prog) + Number.EPSILON) * 100)}%`}</div></div>];
     }
     return (
         <div>
