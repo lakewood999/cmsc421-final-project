a39f4b802251dc2b3b4e5906d98f723fdf29d3d3
Shasank Patel <77682340+corruptcookie@users.noreply.github.com>
2024-05-03 18:33:36 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,24 @@
-import pandas as pd
+import flair
+from flair.data import Sentence
+from flair.nn import Classifier
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoConfig
+from nltk.sentiment import SentimentIntensityAnalyzer
 
+import pandas as pd
+from scipy.special import softmax
+
+# Dataset of reddit comments with an analysis score of 1, 0, or -1
 df = pd.read_csv('Reddit_Data.csv')
+
+# Load the sentiment analysis model
+model_path = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+config = AutoConfig.from_pretrained(model_path)
+# PT
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+# Load the sentiment analysis model
+tagger = Classifier.load('sentiment')
 
 print(df)
