3f61d6acde16484aac716e16a7c62eccd7fb0b29
Steven Su <lakewood999@gmail.com>
2024-05-05 14:09:06 -0400

    Allow hiding/showing comments

--- js_src/src/DisplayPost.tsx
+++ js_src/src/DisplayPost.tsx
@@ -5,9 +5,11 @@ import DisplayComment from "./DisplayComment";
 import Markdown from 'react-markdown'
 import SentimentDisplay from "./SentimentDisplay";
 
-const DisplayPost = (props: { postId: string }) => {
+const DisplayPost = (props: { postId: string, idx: number }) => {
     const data = useDataStore((state) => state.redditData);
     const { nested, parentData } = dataToNested(data);
+
+    const [showComments, setShowComments] = React.useState((props.idx === 0) ? true : false);
 
     const post = parentData[props.postId]
     const children = nested[props.postId]
@@ -28,11 +30,16 @@ const DisplayPost = (props: { postId: string }) => {
                     <Markdown>{post.body === '' ? "<No body text found>" : post.body}</Markdown>
                 </li>
                 <li className="list-group-item">
-                    {children.map((child) => {
-                        return (
-                            <DisplayComment commentId={child.id} />
-                        )
-                    })}
+                    <button className="btn btn-primary mb-3" onClick={() => setShowComments(!showComments)} data-bs-toggle="collapse" data-bs-target={`#comments-of-${props.postId}`}>
+                        {showComments ? "Hide" : "Show"} Comments
+                    </button>
+                    <div className={`collapse ${(showComments ? "show" : "")}`} id={`comments-of-${props.postId}`}>
+                        {children.map((child) => {
+                            return (
+                                <DisplayComment commentId={child.id} />
+                            )
+                        })}
+                    </div>
                 </li>
             </ul>
         </div>
